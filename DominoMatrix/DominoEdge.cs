fa798de6bfe7993e6bb316f2b0b4d742171e3bcd
PAVELchicharacha <pavelkiprov@yandex.ru>
2024-04-26 23:54:43 +0200

    написал классы вершин и точек

---
+++ DominoMatrix/DominoEdge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoMatrix
+{
+    internal class DominoEdge
+    {
+        //ребро(соединялка для точек)
+        public DominoVertex ConVertex { get; }
+       
+
+        public DominoEdge(DominoVertex convertex)//конструктор класса DominoEdge, который принимает один аргумент типа DominoVertex и присваивает его полю ConVertex класса DominoEdge.
+        {
+            ConVertex = convertex;
+        }
+    }
+}
---
+++ DominoMatrix/DominoVertex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoMatrix
+{
+    internal class DominoVertex
+    {
+        //вершина(точки)
+        public string Name { get; }
+        public List<DominoEdge> Edges { get;}
+        public DominoVertex(string name)
+        {
+            Name = name;
+            Edges = new List<DominoEdge>();
+        }
+        public void AddEdge(DominoEdge newEdge)//добавление ребер
+        {
+            Edges.Add(newEdge);
+        }
+        public void AddEdge(DominoVertex vertex)//добавление новых вершин в матрицу
+        {
+            AddEdge(new DominoEdge(vertex));
+        }
+        public override string ToString()=>Name;
+
+    }
+}
